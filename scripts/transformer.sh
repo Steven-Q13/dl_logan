a7a0540889045cba96b7ed230b474377a226b484
Steven Queen <squeen0@umd.edu>
2022-04-25 19:12:30 -0400

    Script change

--- scripts/transformer.sh
+++ scripts/transformer.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 #SBATCH --output=/cfarhomes/squeen0/output_logan/train.out.%j
 #SBATCH --error=/cfarhomes/squeen0/output_logan/train.out.%j
-#SBATCH --time=70:00:00
-#SBATCH --gres=gpu:p6000:4
+#SBATCH --time=30:00:00
+#SBATCH --gres=gpu:p6000:3
 #SBATCH --account=vulcan
-#SBATCH --mem=240000
+#SBATCH --mem=48000
 #SBATCH --partition=scavenger
 #SBATCH --qos=scavenger
 
@@ -12,5 +12,6 @@
 # Check execution with: squeue -u squeen0
 module add Python3/3.8.2
 module add cuda
-srun bash -c "source /cfarhomes/squeen0/dl_logan/venv/bin/activate && python3 /cfarhomes/squeen0/dl_logan/signal_gen/train_transformer.py vulcan 75000 32 5 6 test1"
+srun bash -c "source /cfarhomes/squeen0/dl_logan/venv/bin/activate && python3 /cfarhomes/squeen0/dl_logan/signal_gen/train_transformer.py vulcan 50000 128 5 4 test1"
 #Args: Location, num_epochs, batch_size, num_layers, num_heads, version
+# change time=70:00:00
